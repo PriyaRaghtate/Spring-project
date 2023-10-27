ae7be85f2f3d33bf01917e77f0e7a233cc2123bb
priya raghtate <priyaraghtate01@gmail.com>
2023-10-27 15:56:46 +0530

    reg login costom code

---
+++ Security_reg_log/src/main/java/com/priya/project/SecurityRegLogApplication.java
@@ -0,0 +1,13 @@
+package com.priya.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityRegLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityRegLogApplication.class, args);
+	}
+
+}
---
+++ Security_reg_log/src/test/java/com/priya/project/SecurityRegLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.priya.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityRegLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
